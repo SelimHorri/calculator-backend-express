a9049744b3d53e0f7b9ace41689c80547526e206
SelimHorri <horri.slim@gmail.com>
2021-05-14 00:28:14 +0100

    hardcode username/password

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen("3000", function() {
   console.log("Listening to port 3000...");
@@ -14,5 +17,25 @@ app.get("/msg", function(req, res) {
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/login", function(req, res) {
+  res.sendFile(__dirname + "/login.html");
+});
+
+app.post("/login", function(req, res) {
+  
+  var username = req.body.username;
+  var password = req.body.password;
+  
+  if (username === "selimhorri" && password === "0000") {
+    console.log("logged in at: " + new Date());
+    res.send("Welcome back " + username.toUpperCase());
+  }
+  else {
+    console.log("incorrect! at: " + new Date());
+    res.redirect("/login");
+  }
+  
 });
 
